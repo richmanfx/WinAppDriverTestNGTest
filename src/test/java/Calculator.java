7801340746af87ae2b6418e64082690fabad6baf
richmanfx <igfwbhty73>
2019-09-15 18:54:43 +0300

    Первый комит. Заготовка build.gradle

---
+++ src/test/java/Calculator.java
@@ -0,0 +1,10 @@
+import io.appium.java_client.windows.WindowsDriver;
+import org.openqa.selenium.WebElement;
+
+public class Calculator {
+
+    private static WindowsDriver CalculatorSession = null;
+    private static WebElement CalculatorResult = null;
+
+
+}
